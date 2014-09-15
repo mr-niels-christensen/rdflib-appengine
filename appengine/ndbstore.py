258af87e69561426d3cf54f99faeecddecaee3e8
Niels Christensen <nchristensen@abdn.ac.uk>
2014-09-15 11:43:23 +0100

    Centralize in LietralTriple and NonLiteralTriple

--- appengine/ndbstore.py
+++ appengine/ndbstore.py
@@ -12,6 +12,9 @@ from google.appengine.ext import ndb
 
 ANY = Any = None
 
+def _blank_or_uri(value, is_blank):
+    return term.BNode(value = value) if is_blank else term.URIRef(value = value)
+
 class RdfGraph(ndb.Model):
     '''Use this a ancestor for all triples etc. in the graph
        The entity itself records pairs of (prefix, namespace)
@@ -22,15 +25,91 @@ class RdfGraph(ndb.Model):
 
 class NonLiteralTriple(ndb.Model):
     rdf_subject = ndb.StringProperty()
+    rdf_subject_is_blank = ndb.BooleanProperty()
     rdf_property = ndb.StringProperty()
     rdf_object = ndb.StringProperty()
+    rdf_object_is_blank = ndb.BooleanProperty()
+    
+    @staticmethod
+    def create(graph_key, s, p, o):
+        assert not isinstance(o, term.Literal), "Trying to store a Literal in a NonLiteralTriple: %s %s %s %s" % (graph_key, s, p, o)
+        return NonLiteralTriple(parent = graph_key, 
+                                rdf_subject = unicode(s),
+                                rdf_subject_is_blank = isinstance(s, term.BNode),
+                                rdf_property = unicode(p),
+                                rdf_object = unicode(o),
+                                rdf_object_is_blank = isinstance(o, term.BNode),
+                                )
+    
+        
+    def toRdflib(self):
+        return (_blank_or_uri(self.rdf_subject, self.rdf_subject_is_blank),
+                term.URIRef(value = self.rdf_property),
+                _blank_or_uri(self.rdf_object, self.rdf_object_is_blank)
+                )
+    
+    @staticmethod
+    def matching_query(graph_key, s, p, o):
+        '''Combines subject, property, nonliteral object into an NDB query.
+           The query is not executed.
+           @param s RDF subject or None (for wildcard)
+           @param p RDF property or None (for wilcard)
+           @param o Nonliteral RDF object or None (for wildcard)
+           @return An NDB query that will find the matching triples when executed.
+        '''
+        candidate_filters = zip([NonLiteralTriple.rdf_subject, #We are assuming that BNode names and URIRef names cannot be the same
+                                 NonLiteralTriple.rdf_property, 
+                                 NonLiteralTriple.rdf_object], 
+                                [s, p, o])
+        filters = [(field == value) for field, value in candidate_filters if value is not None]
+        return NonLiteralTriple.query(ancestor = graph_key).filter(*filters)
     
 class LiteralTriple(ndb.Model):
     rdf_subject = ndb.StringProperty()
+    rdf_subject_is_blank = ndb.BooleanProperty()
     rdf_property = ndb.StringProperty()
     rdf_object_lexical = ndb.StringProperty()
     rdf_object_datatype = ndb.StringProperty()
     rdf_object_language = ndb.StringProperty()
+    
+    @staticmethod
+    def create(graph_key, s, p, o):
+        assert isinstance(o, term.Literal), "Trying to store a Literal in a NonLiteralTriple: %s %s %s %s" % (graph_key, s, p, o)
+        return NonLiteralTriple(parent = graph_key, 
+                                rdf_subject = unicode(s),
+                                rdf_subject_is_blank = isinstance(s, term.BNode),
+                                rdf_property = unicode(p),
+                                rdf_object_lexical = unicode(o),
+                                rdf_object_datatype = o.datatype(),
+                                rdf_object_language = o.language()
+                                )
+        
+    def toRdflib(self):
+        return (_blank_or_uri(self.rdf_subject, self.rdf_subject_is_blank),
+                term.URIRef(value = self.rdf_property),
+                term.Literal(self.rdf_object_lexical, 
+                             lang = self.rdf_object_language, 
+                             datatype = self.rdf_object_datatype)
+                )    
+    
+    @staticmethod
+    def matching_query(graph_key, s, p, o):
+        '''Combines subject, property, literal object into an NDB query.
+           The query is not executed.
+           @param s RDF subject or None (for wildcard)
+           @param p RDF property or None (for wilcard)
+           @param o Literal RDF object or None (for wildcard)
+           @return An NDB query that will find the matching triples when executed.
+        '''
+        candidate_filters = zip([NonLiteralTriple.rdf_subject,  #We are assuming that BNode names and URIRef names cannot be the same
+                                 NonLiteralTriple.rdf_property], 
+                                [s, p])
+        filters = [(field == value) for field, value in candidate_filters if value is not None]
+        if o is not None:
+            filters += [LiteralTriple.rdf_object_lexical == unicode(o),
+                        LiteralTriple.rdf_object_datatype == o.datatype,
+                        LiteralTriple.rdf_object_language == o.language]
+        return LiteralTriple.query(ancestor = graph_key).filter(*filters)
 
 class NDBStore(Store):
     """\
@@ -52,17 +131,9 @@ class NDBStore(Store):
             assert c is None, \
                 "Context associated with %s %s %s is not None but %s!" % (s, p, o, c)
         #Convert rdflib tirples to NDB triples
-        lit_triples = [LiteralTriple(parent = self._graph_key, 
-                                     rdf_subject = unicode(s),
-                                     rdf_property = unicode(p),
-                                     rdf_object_lexical = unicode(o),
-                                     rdf_object_datatype = o.datatype,
-                                     rdf_object_language = o.language) 
+        lit_triples = [LiteralTriple.create(self._graph_key, s, p, o) 
                        for (s, p, o, _) in quads if isinstance(o, term.Literal)]
-        nonlit_triples = [NonLiteralTriple(parent = self._graph_key, 
-                                           rdf_subject = unicode(s),
-                                           rdf_property = unicode(p),
-                                           rdf_object = unicode(o)) 
+        nonlit_triples = [NonLiteralTriple.create(self._graph_key, s, p, o)
                           for (s, p, o, _) in quads if isinstance(o, term.Literal)]
         #Insert all the triples in one operation because NDB may allow only one write operation per graph per second. 
         #See bottom of https://developers.google.com/appengine/docs/python/datastore/structuring_for_strong_consistency
@@ -77,27 +148,22 @@ class NDBStore(Store):
     def remove(self, (s, p, o), context=None):
         assert context is None, "Context not supported"
         if isinstance(o, term.Literal):
-            query = (
-                LiteralTriple
-                .query(ancestor = self._graph_key)
-                .filter(ndb.AND(LiteralTriple.rdf_object_lexical == unicode(o),
-                                LiteralTriple.rdf_object_datatype == o.datatype,
-                                LiteralTriple.rdf_object_language == o.language))
-                  )
+            query = LiteralTriple.matching_query(self._graph_key, s, p, o)
         else:
-            query = (
-                      NonLiteralTriple
-                      .query(ancestor = self._graph_key)
-                      .filter(NonLiteralTriple.triple_object == unicode(o))
-                      )
-        for key in query.run(keys_only = True):
+            query = NonLiteralTriple.matching_query(self._graph_key, s, p, o)
+        for key in query.iter(keys_only = True):
             key.delete()
 
     def triples(self, (s, p, o), context=None):
         """A generator over all the triples matching """
-        assert False, "Not implemented yet"
+        assert context is None, "Context associated with %s %s %s is not None but %s!" % (s, p, o, context)
+        if o is None or isinstance(o, term.Literal):
+            for item in LiteralTriple.matching_query(self._graph_key, s, p, o):
+                yield item.toRdflib()
+        if o is None or not isinstance(o, term.Literal):
+            for item in NonLiteralTriple.matching_query(self._graph_key, s, p, o):
+                yield item.toRdflib()
         
-
     def __len__(self, context=None):
         assert False, "Not implemented yet"
 
