8407978b1ec4e21a702d6c8040c648cc23617cdf
Niels Christensen <nchristensen@abdn.ac.uk>
2014-09-22 12:14:48 +0100

    Functional unit test For #1-unittest

--- test/suite/testNdbstore.py
+++ test/suite/testNdbstore.py
@@ -1,7 +1,7 @@
 import unittest
 from appengine import ndbstore
 from google.appengine.ext import testbed
-from google.appengine.datastore import datastore_stub_util
+from rdflib.term import URIRef, Literal
 
 class TestCase(unittest.TestCase):
     def setUp(self):
@@ -9,14 +9,21 @@ class TestCase(unittest.TestCase):
         self.testbed = testbed.Testbed()
         # Then activate the testbed, which prepares the service stubs for use.
         self.testbed.activate()
-        self.testbed. init_datastore_v3_stub()
+        self.testbed.init_datastore_v3_stub()
         self.testbed.init_memcache_stub()
 
     def tearDown(self):
         self.testbed.deactivate()
 
     def testConstructor(self):
+        ndbstore.NDBStore(identifier = 'banana')
+        
+    def testSimpleStoreAndRetrieve(self):
         s = ndbstore.NDBStore(identifier = 'banana')
+        t = (URIRef('http://s'), URIRef('http://p'), Literal('o'))
+        s.add(t, None)
+        self.assertEquals(1, len(s))
+        self.assertEquals([t], [t for (t, _) in s.triples((None, None, None), None)])
 
 if __name__ == '__main__':
     unittest.main()
