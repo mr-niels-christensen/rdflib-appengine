4ff1cb5888be85732c3242d14e073335e5f24506
Niels H. Christensen <nhc@mayacs.com>
2014-11-25 12:43:22 +0000

    Imported query evaluation from client project For #8-eval-to-store

--- appengine/ndbstore.py
+++ appengine/ndbstore.py
@@ -19,6 +19,8 @@ from rdflib.plugins.memory import IOMemory
 from weakref import WeakValueDictionary
 from StringIO import StringIO
 from random import randrange
+from rdflib.plugins.sparql.evaluate import evalPart
+from rdflib.plugins.sparql import CUSTOM_EVALS
 
 ANY = None
 
@@ -363,3 +365,50 @@ class CoarseNDBStore(Store):
         '''
         if False:
             yield
+# ------------------------------------------------------------------------
+# Modify query evaluation with CoarseNDBStores in rdflib
+ 
+def _evalLazyJoin(ctx, join):
+    """
+    A lazy join will push the variables bound
+    in the first part to the second part,
+    essentially doing the join implicitly
+    hopefully evaluating much fewer triples
+    """
+    for a in evalPart(ctx, join.p1):
+        c = ctx.thaw(a)
+        for b in evalPart(c, join.p2):
+            yield b
+
+def _evalPartLogger(ctx, part):
+    if not isinstance(ctx.graph.store, CoarseNDBStore):
+        raise NotImplementedError
+    if part.name == 'SelectQuery':
+        s = StringIO()
+        _dump(part, '', s)
+        ctx.graph.store.log(s.getvalue())
+        raise NotImplementedError
+    elif part.name == 'Join':
+        return _evalLazyJoin(ctx, part)
+    else:
+        raise NotImplementedError
+
+def _dump(part, indent, dest):
+    if part is None:
+        return None
+    if part.name == 'BGP':
+        dest.write('{}{} {} triples={}\n'.format(indent, part.name, id(part) % 10000, part.triples))
+        return
+    if part.name == 'Extend':
+        dest.write('{}{} {} {}={}\n'.format(indent, part.name, id(part) % 10000, part.var, part.expr))
+    else:
+        dest.write('{}{} {}\n'.format(indent, part.name, id(part) % 10000))
+    for attr in ['p', 'p1', 'p2']:
+        if hasattr(part, attr):
+            child  = getattr(part, attr)
+            if child is not None:
+                _dump(child, '{}  '.format(indent), dest)
+    return
+
+CUSTOM_EVALS['querylog'] = _evalPartLogger
+logging.info('Activated specialized query evaluation in rdflib')
